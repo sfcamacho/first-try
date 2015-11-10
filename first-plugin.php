1dba895852ee557a107735ad1640416b3e1f06ed
S. Camacho <scamacho.k2c@gmail.com>
2015-11-10 14:24:17 +0000

    VFI.

---
+++ first-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Deploy WordPress Plugins With GitHub
+ *
+ * @package SFC
+ *
+ * Plugin Name: First
+ * Description: Just a testing the Matthew Ray article To deploy plugins with GitHub
+ * Version: 0.1.0
+ * Author: SFC 
+ * Author URI: http://smashingmagazine.com/2015/08/deploy-wordpress-plugins-with-github-using-transients/
+ * License: GPLv2 or later
+
